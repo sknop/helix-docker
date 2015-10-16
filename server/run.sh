ec9418fe2d17e71fdc74491885cce56ef3a9ea57
Sven Erik Knop <sknop@perforce.com>
2015-10-16 15:51:39 +0100

    Do not overwrite existing P4ROOT, just start the server
    If P4ROOT is empty (i.e. db.rev is not there), initialize
    with default values.

--- server/run.sh
+++ server/run.sh
@@ -24,39 +24,45 @@ P4PASSWD=${P4PASSWD:-Password}
 
 # set up directories
 
-mkdir -p $P4ROOT
+if [ -f $P4ROOT/db.rev ]; then
+    echo "$P4ROOT/db.rev already exists, bugging out of here"
+else 
+    echo Setting up $P4ROOT ---------------------------------------
+    mkdir -p $P4ROOT
 
-# set up P4CONFIG for RSH hack
+    # set up P4CONFIG for RSH hack
 
-export P4CONFIG=.p4
-mkdir -p /client
-cat >> /client/.p4 <<!
+    export P4CONFIG=.p4
+    mkdir -p /client
+    cat >> /client/.p4 <<!
 P4PORT=rsh:/opt/perforce/sbin/p4d -r $P4ROOT -i -L P4LOG
 P4USER=$P4USER
 P4CLIENT=setup.client
 !
 
-# configure server
+    # configure server
 
-cd /client
-p4 user -f -o | p4 user -i
-p4 client -o  | p4 client -i
+    cd /client
+    p4 user -f -o | p4 user -i
+    p4 client -o  | p4 client -i
 
-echo Setting password to $P4PASSWD
+    echo Setting password to $P4PASSWD
 
-p4 passwd <<!
+    p4 passwd <<!
 $P4PASSWD
 $P4PASSWD
 !
 
-p4 login <<!
+    p4 login <<!
 $P4PASSWD
 !
 
-# security settings
-p4 configure set dm.user.noautocreate=1
-p4 protect -o | p4 protect -i
-p4 configure set security=3
+    # security settings
+    p4 configure set dm.user.noautocreate=1
+    p4 protect -o | p4 protect -i
+    p4 configure set security=3
+
+fi
 
 # start server
 
