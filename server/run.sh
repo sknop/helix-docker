3a2d392e4c9776922d20202397805d0e1a5ac48c
Sven Erik Knop <sknop@perforce.com>
2015-10-16 17:24:42 +0100

    Bit of clean-up of run.sh to make it easier to read and debug

--- server/run.sh
+++ server/run.sh
@@ -4,44 +4,33 @@ set -e
 # P4PORT provided via environment
 # P4ROOT provided via environment
 
-if [ -z $P4PORT ]; then
-    echo "P4PORT must be defined"
-    exit 1
-fi
+function check_environment {
+    if [ -z $P4PORT ]; then
+        echo "P4PORT must be defined"
+        exit 1
+    fi
 
-if [ -z $P4ROOT ]; then
-    echo "P4ROOT must be defined"
-    exit 1
-fi
+    if [ -z $P4ROOT ]; then
+        echo "P4ROOT must be defined"
+        exit 1
+    fi
 
-if [ -z $P4LOG ]; then
-    echo "P4LOG must be defined"
-    exit 1
-fi
+    if [ -z $P4LOG ]; then
+        echo "P4LOG must be defined"
+        exit 1
+    fi
+}
 
-P4USER=${P4USER:-p4admin}
-P4PASSWD=${P4PASSWD:-Password}
-
-# set up directories
-
-if [ -f $P4ROOT/db.rev ]; then
-    echo "$P4ROOT/db.rev already exists, bugging out of here"
-else 
-    echo Setting up $P4ROOT ---------------------------------------
-    mkdir -p $P4ROOT
-
-    # set up P4CONFIG for RSH hack
-
-    export P4CONFIG=.p4
+function create_config {
     mkdir -p /client
     cat >> /client/.p4 <<!
 P4PORT=rsh:/opt/perforce/sbin/p4d -r $P4ROOT -i -L P4LOG
 P4USER=$P4USER
 P4CLIENT=setup.client
 !
+}
 
-    # configure server
-
+function setup_server {
     cd /client
     p4 user -f -o | p4 user -i
     p4 client -o  | p4 client -i
@@ -61,7 +50,19 @@ $P4PASSWD
     p4 configure set dm.user.noautocreate=1
     p4 protect -o | p4 protect -i
     p4 configure set security=3
+}
 
+check_environment
+
+P4USER=${P4USER:-p4admin}
+P4PASSWD=${P4PASSWD:-Password}
+export P4CONFIG=.p4
+
+if [ -f $P4ROOT/db.rev ]; then
+    echo "$P4ROOT/db.rev already exists, bugging out of here"
+else 
+    create_config
+    setup_server
 fi
 
 # start server
