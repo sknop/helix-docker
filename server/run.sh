bd1a7f8e8962445c1213680093f5e4cf80699e89
Sven Erik Knop <sknop@perforce.com>
2015-10-22 15:22:50 +0100

    Configuration shifted into Python script

--- server/run.sh
+++ server/run.sh
@@ -21,50 +21,15 @@ function check_environment {
     fi
 }
 
-CLIENT=/opt/perforce/client
-
-function create_config {
-    mkdir -p ${CLIENT}
-    cat >> ${CLIENT}/.p4 <<!
-P4PORT=rsh:/opt/perforce/sbin/p4d -r $P4ROOT -i -L P4LOG
-P4USER=$P4USER
-P4CLIENT=setup.client
-!
-}
-
-function setup_server {
-    cd ${CLIENT}
-    p4 user -f -o | p4 user -i
-    p4 client -o  | p4 client -i
-
-    echo Setting password to $P4PASSWD
-
-    p4 passwd <<!
-$P4PASSWD
-$P4PASSWD
-!
-
-    p4 login <<!
-$P4PASSWD
-!
-
-    # security settings
-    p4 configure set dm.user.noautocreate=1
-    p4 protect -o | p4 protect -i
-    p4 configure set security=3
-}
-
 check_environment
 
 P4USER=${P4USER:-p4admin}
 P4PASSWD=${P4PASSWD:-Password}
-export P4CONFIG=.p4
 
 if [ -f $P4ROOT/db.rev ]; then
     echo "$P4ROOT/db.rev already exists, bugging out of here"
 else 
-    create_config
-    setup_server
+    python2.7 /SetupHelix.py -p "rsh:/opt/perforce/sbin/p4d -r $P4ROOT -i -L P4LOG" -u $P4USER -P $P4PASSWD
 fi
 
 # start server
